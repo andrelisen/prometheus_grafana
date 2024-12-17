8ba0ed191e491a275aefc87f6273d1bfc45cfd31
Andrelise <andrelisenuneslemos@gmail.com>
2024-12-17 07:16:54 -0300

    Add metrica do tipo gauge com valor aleatorio

--- app/index.js
+++ app/index.js
@@ -9,12 +9,19 @@ const counter = new prom.Counter({
     name: 'aula_request_total',
     help: 'Contador de requests',
     labelNames: ['statusCode']
-})
+});
 
+const gauge = new prom.Gauge({
+    name: 'aula_free_bytes',
+    help: 'Exemplo de Gauge'
+});
 
 app.get('/', function (req, res) {
     //contando a quantidade de requests
     counter.labels('200').inc();
+
+    //cria uma metrica do tipo gauge com um valor aleatorio
+    gauge.set(100*Math.random());
     
     res.send('Instrumentando uma app com o Prometheus!');
 });
