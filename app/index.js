c5f9936d1ba8e89b0029d15972f14df7252b1e1c
Andrelise <andrelisenuneslemos@gmail.com>
2024-12-17 07:13:02 -0300

    Add metrica que conta a qntdade de requests em uma rota

--- app/index.js
+++ app/index.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var prom = require('prom-client');
+const register = prom.register;
 
 var app = express();
 
-//
-const client = require('prom-client');
-const counter = new client.Counter({
+
+const counter = new prom.Counter({
     name: 'aula_request_total',
-    help: 'Contador de requests'
+    help: 'Contador de requests',
+    labelNames: ['statusCode']
 })
 
 
 app.get('/', function (req, res) {
-    counter.inc();
-
+    //contando a quantidade de requests
+    counter.labels('200').inc();
+    
     res.send('Instrumentando uma app com o Prometheus!');
 });
 
+//criando a rota para externalizar o /metrics para o prometheus
+app.get('/metrics', async function (req, res) {  
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+});
+
+//Porta sendo externalizada
 app.listen(3000);
